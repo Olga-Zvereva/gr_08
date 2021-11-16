5f42a2dca601b442932103c7bba412d92427682d
Alexander Zaikin <ay.zaikin@gmail.com>
2021-11-16 17:55:11 +0500

    Этап 2: добавлено управление размером мозаики и градациями серого, рефакторинг

--- filter.py
+++ filter.py
@@ -1,28 +1,41 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i <= a - 10:
-    j = 0
-    while j <= a1 - 10:
-        s = 0
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                n1 = int(arr[x][y][0])
-                n2 = int(arr[x][y][1])
-                n3 = int(arr[x][y][2])
-                M = (n1 + n2 + n3) / 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def get_average(kernel):
+    average = 0
+    for x in range(KERNEL_SIZE):
+        for y in range(KERNEL_SIZE):
+            red = int(kernel[x][y][0])
+            green = int(kernel[x][y][1])
+            blue = int(kernel[x][y][2])
+            average += (red + green + blue) / 3
+    average = average / (KERNEL_SIZE*KERNEL_SIZE)
+    average = int(average//STEP_SIZE) * STEP_SIZE
+    return average
+
+
+def fill_with(value, kernel):
+    for x in range(KERNEL_SIZE):
+        for y in range(KERNEL_SIZE):
+            kernel[x][y][0] = value
+            kernel[x][y][1] = value
+            kernel[x][y][2] = value
+
+
+KERNEL_SIZE = 10
+STEP_COUNT = 5
+STEP_SIZE = 256 // STEP_COUNT
+input_image = Image.open("img2.jpg")
+image = np.array(input_image)
+width = len(image)
+height = len(image[1])
+
+for x in range(0, width-KERNEL_SIZE+1, KERNEL_SIZE):
+    for y in range(0, height-KERNEL_SIZE+1, KERNEL_SIZE):
+        kernel = image[x : x+KERNEL_SIZE, y : y+KERNEL_SIZE]
+        average = get_average(kernel)
+        fill_with(average, kernel)
+
+result_image = Image.fromarray(image)
+result_image.save('res.jpg')
