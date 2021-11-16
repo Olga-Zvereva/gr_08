0b84cdfead07b943ad93ea030bc1ccffd85baea8
VladimirMilch <80639863+VladimirMilch@users.noreply.github.com>
2021-11-17 01:33:33 +0500

    этап 2

--- filter.py
+++ filter.py
@@ -2,28 +2,41 @@ from PIL import Image
 import numpy as np
 
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for k in range(j, j + 10):
-                n1 = arr[n][k][0]
-                n2 = arr[n][k][1]
-                n3 = arr[n][k][2]
-                M = int(n1) + int(n2) + int(n3)
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for k in range(j, j + 10):
-                arr[n][k][0] = int(s // 50) * 50
-                arr[n][k][1] = int(s // 50) * 50
-                arr[n][k][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+pixels_array = np.array(img)
+width = len(pixels_array)
+height = len(pixels_array[1])
+width_mosaic = 10
+grad = 50
+
+
+def get_brightness_gray(pixels_array, i, j, width_mosaic):
+    final_brightness = 0
+    for row in range(i, i + width_mosaic):
+        for column in range(j, j + width_mosaic):
+            red = pixels_array[row][column][0]
+            green = pixels_array[row][column][1]
+            blue = pixels_array[row][column][2]
+            color = int(red) + int(green) + int(blue)
+            final_brightness += color // 3
+    return int(final_brightness // width_mosaic ** 2)
+
+
+def get_mosaic(pixels_array, grad, width_mosaic):
+    i = 0
+    while i < width:
+        j = 0
+        while j < height:
+            average_brightness = get_brightness_gray(pixels_array, i, j, width_mosaic)
+            for row in range(i, i + width_mosaic):
+                for column in range(j, j + width_mosaic):
+                    pixels_array[row][column][0] = int(average_brightness // grad) * grad
+                    pixels_array[row][column][1] = int(average_brightness // grad) * grad
+                    pixels_array[row][column][2] = int(average_brightness // grad) * grad
+            j = j + width_mosaic
+        i = i + width_mosaic
+    return pixels_array
+
+
+pixels_array = get_mosaic(pixels_array, grad, width_mosaic)
+result_image = Image.fromarray(pixels_array)
+result_image.save('res.jpg')
