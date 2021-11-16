e20793e4e0275a2d429d02b7ab82572aadb3f43c
VladimirMilch <80639863+VladimirMilch@users.noreply.github.com>
2021-11-16 17:29:46 +0500

    Этап 1

--- filter.py
+++ filter.py
@@ -1,28 +1,35 @@
 from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 11:
-    j = 0
-    while j < a1 - 11:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+
+def func_average_brightness(pixels_array, mosaic, i, j):
+    area = pixels_array[i:i + mosaic, j:j + mosaic]
+    final_brightness = np.sum(area)
+    return int(final_brightness // 3 // mosaic ** 2)
+
+
+def new_pixels_color(pixels_array, mosaic, grayscale, average_brightness, i, j):
+    pixel_value = int(average_brightness // grayscale) * grayscale
+    pixels_array[i:i + mosaic, j:j + mosaic] = pixel_value
+    return pixels_array
+
+
+def grey_filter(input_image, mosaic, grayscale):
+    pixels_array = np.array(input_image)
+    height = len(pixels_array)
+    width = len(pixels_array[1])
+    for i in range(0, height - mosaic + 1, mosaic):
+        for j in range(0, width - mosaic + 1, mosaic):
+            average_brightness = func_average_brightness(pixels_array, mosaic, i, j)
+            pixels_array = new_pixels_color(pixels_array, mosaic, grayscale, average_brightness, i, j)
+    return pixels_array
+
+
+def main():
+    name_input_image = '{}.{}'.format(input('Имя входного изображения - '), input('Формат входного изображения - '))
+    mosaic = int(input('Размер мозаики - '))
+    grayscale = int(input('Величина градации серого - '))
+    name_output_image = '{}.{}'.format(input('Имя выходного изображения - '), input('Формат выходного изображения - '))
+    input_image = Image.open(name_input_image)
+    gray_image = Image.fromarray(grey_filter(input_image, mosaic, grayscale))
+    gray_image.save(name_output_image)
